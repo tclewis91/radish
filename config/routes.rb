0cae0dd2ebc0693d2ec2d17d59f8763567f3ca4e
tclewis91 <tclewis91@gmail.com>
2015-08-17 13:12:17 -0500

    votable methods added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # resources :links do
-  #   member do
-  #     put 'like', to: 'posts#upvote'
-  #     put 'unlike', to: 'posts#downvote'
-  #   end
-  # end
+  resources :links do
+    member do
+      put 'like', to: 'posts#upvote'
+      put 'unlike', to: 'posts#downvote'
+    end
+  end
 
   root "links#index"
 end
